fd2d0c1a0da10414b892f44ec602b83963b19907
Aldo Matias <aldo_rheazyk@hotmail.com>
2021-12-13 20:47:14 -0600

    [UPDATE] Add n lines

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -10,7 +10,7 @@ export default function Table({
     const [currentPage, setCurrentPage] = useState(1);
 
     const dataFilter = useMemo(() => {
-        const newData =  data.filter((item, index) => index < currentPage * 10 && index >= (currentPage - 1) * 10) ;
+        const newData = data.filter((item, index) => index < currentPage * 10 && index >= (currentPage - 1) * 10);
         return newData;
     }, [data, currentPage]);
 
@@ -28,39 +28,39 @@ export default function Table({
         }
     }
 
-    const getItem = (item)=>{
-        return data.find(element => +element.id===item.id)?.pepe;
+    const getItem = (item) => {
+        return data.find(element => +element.id === item.id)?.pepe;
     }
 
-    const getIndexCity = ()=>{
-        return data.findIndex(element => element.cityid==="MXAS0175");
+    const getIndexCity = () => {
+        return data.findIndex(element => element.cityid === "MXAS0175");
     }
 
     let clicked = [];
 
-    const addSameCity = (item)=>{
+    const addSameCity = (item) => {
         clicked.push(item);
     }
-    
-    
+
+
 
     const handleSelectCity = (element) => {
         var nombre = "Juan";
         //const nombre =  "Aldo";
 
-        console.log(nombre);
+       /*  console.log(nombre); */
         //console.log(item);
 
 
         const indexCity = getIndexCity();
         //console.log(indexCity);
-        if(indexCity>=0){
+        if (indexCity >= 0) {
             console.log("CONGRATULATIONS!");
         }
 
         const filterM = clicked.filter(item => item.cityid === element.cityid);//RETORNA UN ARRAY CON LOS ELEMENTOS QUE COINCIDEN CON LA CIUDAD, RETORNA UN ARRAY VACIO SI NO ENCUENTRA NADA
-        const findM= clicked.find(item => item.cityid === element.cityid);//RETORNA UN OBJETO ELEMENTO QUE COINCIDEN CON LA CIUDAD, RETORNA UNDEFINED SI NO ENCUENTRA NADA
-        const findIndexM= clicked.findIndex(item => item.cityid === element.cityid);//RETORNA UN NUMERO QUE COINCIDEN CON LA CIUDAD, RETORNA -1 SI NO ENCUENTRA NADA
+        const findM = clicked.find(item => item.cityid === element.cityid);//RETORNA UN OBJETO ELEMENTO QUE COINCIDEN CON LA CIUDAD, RETORNA UNDEFINED SI NO ENCUENTRA NADA
+        const findIndexM = clicked.findIndex(item => item.cityid === element.cityid);//RETORNA UN NUMERO QUE COINCIDEN CON LA CIUDAD, RETORNA -1 SI NO ENCUENTRA NADA
 
         /* console.log(filterM);
         console.log(filterM.id); *///ACCEDIENDO A UNA PROPIEDAD DE UN OBJETO
@@ -73,76 +73,179 @@ export default function Table({
         /* console.log(findM);
         console.log(findM);
 
+
+    
+
         if(!findM){ //FIND       {}= A TRUE PARA EL IF 
             addSameCity(element);
         }else{ //UNDEFINED FALSE PARA EL IF
             console.log("YA EXISTE");
         } */
 
-        if(findIndexM===-1){ //FINDINDEX       {}= A TRUE PARA EL IF 
+        if (findIndexM === -1) { //FINDINDEX       {}= A TRUE PARA EL IF 
             addSameCity(element);
-        }else{ //UNDEFINED FALSE PARA EL IF
-            console.log("YA EXISTE");
+        } else { //UNDEFINED FALSE PARA EL IF
+            /* console.log("YA EXISTE"); */
         }
 
-        console.log(clicked);
-        
+
+
+
+
+/* 
+        console.log(clicked); */
+
         //setElementSelect(element);
-    }
-    console.log(clicked);
-    var nombre="Oscar";
-    console.log(nombre);
+    }/* 
+    console.log(clicked); */
+    var nombre = "Oscar";/* 
+    console.log(nombre); */
 
     const handleDelete = (city) => {
         const item = {};
-        setData(prevState => prevState.filter(item => !(+item.relativehumidity > 80 && parseInt(item.relativehumidity) <90)));
+        setData(prevState => prevState.filter(item => !(+item.relativehumidity > 80 && parseInt(item.relativehumidity) < 90)));
     }
-    
-    
+
+    /* for (let i = data.length; i >= data.length / 2; i--) {
+        /* console.log(data[i]); 
+    }
+ */
+    const [aguascalientess, setAguas] = useState([]);
+
+
+    const [openTable, setOpenTable] = useState(false);
+
+    const handleLastDelete = (e) => {
+        /* setOpenTable(true); //openTable = true */
+
+        console.log(openTable);
+        setOpenTable(prevState=> !prevState); //openTable = false */
+
+
+        /* setOpenTable(!openTable);//openTable = false */
+
+        console.log(openTable); 
+
+     setAguas(prevState => [
+            ...prevState,
+            {   ///EQUIVALENTE PUSH 
+                "_id": "5952983359954a0adbf7ab01a",
+                "cityid": "MXAS01701",
+                "validdateutc": "20170627T140000Z",
+                "winddirectioncardinal": "S",
+                "probabilityofprecip": "60",
+                "relativehumidity": "91",
+                "name": "Asientos",
+                "date-insert": "2017-06-27T17:36:43.088Z",
+                "longitude": "-102.0893",
+                "state": `Aguascalientes TEST ${aguascalientess.length} sss`,
+                "lastreporttime": "20170627T092453Z",
+                "skydescriptionlong": "Tormentas dispersas",
+                "stateabbr": "AGU",
+                "tempc": "15",
+                "latitude": "22.23832",
+                "iconcode": "96",
+                "windspeedkm": "5",
+                "index": 1,
+                "claseOscar": "22-NOV-2021",
+                "id": 0.29332035235540654
+                },
+        ]
+        );
+
+        setAguas(prevState => [{   ///EQUIVALENTE shift 
+            "_id": "5952983359954a0adbf7ab01a",
+            "cityid": "MXAS01701",
+            "validdateutc": "20170627T140000Z",
+            "winddirectioncardinal": "S",
+            "probabilityofprecip": "60",
+            "relativehumidity": "91",
+            "name": "Asientos",
+            "date-insert": "2017-06-27T17:36:43.088Z",
+            "longitude": "-102.0893",
+            "state": `Aguascalientes TEST ${aguascalientess.length} sss`,
+            "lastreporttime": "20170627T092453Z",
+            "skydescriptionlong": "Tormentas dispersas",
+            "stateabbr": "AGU",
+            "tempc": "15",
+            "latitude": "22.23832",
+            "iconcode": "96",
+            "windspeedkm": "5",
+            "index": 1,
+            "claseOscar": "22-NOV-2021",
+            "id": 0.29332035235540654
+            },
+            ...prevState,
+            
+        ]
+        );
+
+
+        /* setAguas(["Hola"]);
+        setAguas(["Mundo"]);
+        setAguas(prevState => prevState.filter((item, index) => index < data.length-1)); */
+        /* data.forEach((element, index) => {
+            if (["Aguascalientes","Baja california","Chihuahua","Coahuila", "Campeche" ].includes(element.state)) {
+                data.push(element);
+            } 
+            /* if (element.state === "Aguascalientes" || element.state === "Baja California" || element.state === "Chihuahua" ) {
+                aguascalientess.push(element);
+            }  
+        }); */
+
+        /* console.log(aguascalientess); */
+    }
+    console.log(aguascalientess); 
+
+
     return (
         <>
             <h1>Condiciones Atmosfericas</h1>
             <div className="row">
                 <div className="buttons-pagination">
                     <div>
-                        <button onClick={handlePrevPage}>Previous Page</button>
+                        <button onClick={handleNextPage}>Previous Page</button>
                     </div>
                     <div>
-                        <button onClick={handleNextPage}>Next Page</button>
+                        <button onClick={handleLastDelete}>Eliminar el ultimo</button>
                     </div>
                 </div>
-                <div className="table-container">
-                    <table className="table-striped">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Ciudad</th>
-                                <th>Nombre</th>
-                                <th>Estado</th>
-                                <th>Probabilida de Precipitacion </th>
-                                <th>Humedad Relativa</th>
-                                <th>Ultimo Reporte</th>
-                                <th>LLUEVE</th>
-                                <th>ELIMINAR</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {dataFilter.map(item => (
-                                <tr key={item.id}>
-                                    <td>{item._id}</td>
-                                    <td onClick={e=>{handleSelectCity(item)}}>{item.cityid}</td>
-                                    <td>{item.name}</td>
-                                    <td>{item.state}</td>
-                                    <td>{item.probabilityofprecip}</td>
-                                    <td>{item.relativehumidity}</td>
-                                    <td>{moment(item.lastreporttime).format("YYYY-MM-DD")}</td>
-                                    <td>{`${item.probabilityofprecip > 60 || item.relativehumidity > 50 ? "SI" : "NO"}`}</td>
-                                    <td><button onClick={e=>handleDelete(item)}>Delete</button></td>
+                {
+                  
+                    <div className="table-container">
+                        <table className="table-striped">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Ciudad</th>
+                                    <th>Nombre</th>
+                                    <th>Estado</th>
+                                    <th>Probabilida de Precipitacion </th>
+                                    <th>Humedad Relativa</th>
+                                    <th>Ultimo Reporte</th>
+                                    <th>LLUEVE</th>
+                                    <th>ELIMINAR</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
+                            </thead>
+                            <tbody>
+                                {dataFilter.map(item => (
+                                    <tr key={item.id}>
+                                        <td>{item._id}</td>
+                                        <td onClick={e => { handleSelectCity(item) }}>{item.cityid}</td>
+                                        <td>{item.name}</td>
+                                        <td>{item.state}</td>
+                                        <td>{item.probabilityofprecip}</td>
+                                        <td>{item.relativehumidity}</td>
+                                        <td>{moment(item.lastreporttime).format("YYYY-MM-DD")}</td>
+                                        <td>{`${item.probabilityofprecip > 60 || item.relativehumidity > 50 ? "SI" : "NO"}`}</td>
+                                        <td><button onClick={e => handleDelete(item)}>Delete</button></td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                }
+
                 <label className="grand-total">Total de registros = {data.length}</label>
             </div>
         </>
